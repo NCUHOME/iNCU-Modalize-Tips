78869f9c15441caaabf8a231d9457c7eacb2de6a
Heuluck <sonicth@foxmail.com>
2026-05-22 01:33:50 +0800

    chore: 修改文案

--- app/routes/add-widget-guide/android/meta.ts
+++ app/routes/add-widget-guide/android/meta.ts
@@ -1,5 +1,5 @@
 export const routeMeta = {
   title: '其他 Android',
-  description: '原生Android添加小组件',
+  description: '其他Android添加小组件',
   image: '/images/android.jpg',
 } as const;
--- app/routes/widget-troubleshoot/android/meta.ts
+++ app/routes/widget-troubleshoot/android/meta.ts
@@ -1,5 +1,5 @@
 export const routeMeta = {
   title: '其他 Android',
-  description: '解决原生Android系统中小组件不刷新问题',
+  description: '解决其他Android系统中小组件不刷新问题',
   image: '/images/android.jpg',
 } as const;
